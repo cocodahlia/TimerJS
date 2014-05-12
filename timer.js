d29c1e641e4f7ca6c8fdde744340a5cd670dd7fc
Dahlia* <dahlia.m77@gmail.com>
2014-05-13 00:25:34 +0900

    音を鳴らすか鳴らさないか選べるようにした
    やっぱり銅鑼の音はうるさい

--- timer.js
+++ timer.js
@@ -1,5 +1,7 @@
 var selectTime = 0;
 var countTime = null;
+var sound = true;
+var counter = 0;
 
 function startTimer(){
 	document.getElementById("start").disabled = "disabled";
@@ -13,14 +15,22 @@ function setTimer(){
 	selectTime = selectTime - 1;
 	document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Stopをおすととまります";
 	if ( selectTime == 0 ){
-		clearInterval( countTime );
-		//alert( "終了ー" );
-		countTime = null;
-		document.getElementById("start").disabled = "";
-		audio = new Audio();
-		audio.src = "ata_a19.wav";
-		audio.play();
-		document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Goをおすとカウントダウンがはじまります";
+		if( sound ){
+			clearInterval( countTime );
+			//alert( "終了ー" );
+			countTime = null;
+			document.getElementById("start").disabled = "";
+			audio = new Audio();
+			audio.src = "ata_a19.wav";
+			audio.play();
+			document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Goをおすとカウントダウンがはじまります";
+		}else{
+			clearInterval( countTime );
+			alert( "終了ー" );
+			countTime = null;
+			document.getElementById("start").disabled = "";
+			document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Goをおすとカウントダウンがはじまります";
+		}
 	}
 }
 
@@ -39,4 +49,19 @@ function getRestTimeText() {
 	var minutes = Math.floor(selectTime / 60);
 	var seconds = selectTime % 60;
 	return "残り時間：" + minutes + "分"+ seconds + "秒";
+}
+
+function soundOff(){
+	if (counter % 2 == 0){
+		sound = false;
+		sound_off.value="ON";
+		document.getElementById( "sound" ).innerHTML = "ボタンをおすと音声を再生するか選べます（現在：OFF）";
+		counter++;
+	}else{
+		sound = true;
+		sound_off.value="OFF"
+		document.getElementById( "sound" ).innerHTML = "ボタンをおすと音声を再生するか選べます（現在：ON）";
+		counter++;
+	}
+
 }
