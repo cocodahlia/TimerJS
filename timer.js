176fa5b00d76d246887704dbde4e0fe03ac03cb6
Dahlia* <dahlia.m77@gmail.com>
2014-05-12 17:08:22 +0900

    JavaScriptとHTMLをわけた
    timer.jsというファイルにタイマーのスクリプトをそのままぶちこんで、外部ファイルを読み込むようにした

---
+++ timer.js
@@ -0,0 +1,38 @@
+var selectTime = 0;
+var countTime = null;
+
+function startTimer(){
+	if(countTime != null){
+		alert( "うごいてるー" );
+	}else{
+		var mySelect = document.Form.selector.selectedIndex;
+		selectTime = document.Form.selector.options[mySelect].value - 0;
+		countTime = setInterval ( setTimer, 1000 );
+	}
+}
+
+function setTimer(){
+	selectTime = selectTime - 1;
+	document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Stopをおすととまります";
+	if ( selectTime == 0 ){
+		clearInterval( countTime );
+		alert( "終了ー" );
+		countTime = null;
+		document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Goをおすとカウントダウンがはじまります";
+	}
+}
+
+function stopTimer(){
+	if(countTime != null){
+		clearInterval(countTime);
+		countTime = null;
+		document.getElementById( "countdown" ).innerHTML = getRestTimeText() + "<br>Goをおすとはじめからカウントダウンしなおします";
+		selectTime = 0;
+	}
+}
+
+function getRestTimeText() {
+	var minutes = Math.floor(selectTime / 60);
+	var seconds = selectTime % 60;
+	return "残り時間：" + minutes + "分"+ seconds + "秒";
+}
